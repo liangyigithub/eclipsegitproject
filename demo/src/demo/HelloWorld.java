1d1a35f8789a47b98f02371433ba3a5bac5fd671
liangyigithub <18727050334@163.com>
2018-09-06 22:55:43 +0800

    第一次发布在码云的项目

--- demo/src/demo/HelloWorld.java
+++ demo/src/demo/HelloWorld.java
@@ -1,9 +1,19 @@
 package demo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.sun.jndi.ldap.sasl.SaslInputStream;
+
 /*
- * ÄãºÃÊÀ½ç
+ * Ã„Ã£ÂºÃƒÃŠÃ€Â½Ã§
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world123");
+		String str = "  000019800  ";
+		System.out.println(str.replaceAll("^(\\s*)(0*)","").trim());
+		 
+
+		
 	}
 }
